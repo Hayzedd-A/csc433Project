1dedca09bb860f2994959e219b2e32d192d62605
Hayzedd-A <adebayoazeez37@yahoo.com>
2023-05-26 07:07:55 +0100

    interface for the project

---
+++ script.js
@@ -0,0 +1,26 @@
+$('button').click(function(id) {
+    if(id.currentTarget['id'] == 'submit') {
+        console.log('submitting')
+    }
+    else {
+        let hrWidth = $('.progressBar').width() / 19
+        let translateValue = $('.allPage').css('transform').split(' ')[5].slice(0, -1) / $(window).height() * 100
+
+        if (id.currentTarget['id'] == 'up' && translateValue < 0) {
+        translateValue += 60
+            $('hr#progress').width($('hr#progress').width() - hrWidth)
+            $('span#currentNumber').text(parseInt($('span#currentNumber').text()) - 1)
+        }
+
+        if (id.currentTarget['id'] == 'down' && translateValue > -60 * 4) {
+            translateValue -= 60
+            $('hr#progress').width($('hr#progress').width() + hrWidth)
+            $('span#currentNumber').text(parseInt($('span#currentNumber').text()) + 1)
+
+        }
+
+        $('.allPage').css('transform', 'translateY('+ translateValue +'vh)')
+
+    }
+})
+
