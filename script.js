84521ec43d64df46b94d220b64a78693177d59c6
Hayzedd-A <adebayoazeez37@yahoo.com>
2023-05-27 08:08:27 +0100

    all interface function completed

--- script.js
+++ script.js
@@ -9,45 +9,50 @@ $('li').click(function() {
     result[pageName] = answer
 })
 
+let hrWidth = ($('.hrBar #background').width() / 19) 
+let currentPage = 1
+
 $('button').click(function(id) {
     if(id.currentTarget['id'] == 'submit') {
         console.log(result)
-        if (result.length == 19) {
+        if (Object.keys(result).length == 19) {
             console.log('submitting to API')
         }
         else {
+            currentPage = 0
             $('.eachPage').each(function() {
                 let pageName = $(this).attr('class').split(' ')[1]
                 if (!(pageName in result)) {
                     console.log('this is not selected' + pageName)
-                    $('html, .allPage').animate({
-                        scrollTop: $('.'+ pageName).offset().top
-                    })
-                    // $('.'+pageName).scrollIntoView({behavior: 'smooth'})
-
+                    $('.allPage').css('transform', 'translateY('+ currentPage * -60 +'vh)')
+                    currentPage ++
+                    return false
+                    
                 }
-                console.log(pageName)
+                currentPage ++
             })
         }
 
     }
     else {
-        let hrWidth = ($('.hrBar #background').width() / 19) + 0.05
+        hrWidth = ($('.hrBar #background').width() / 19) 
         let translateValue = $('.allPage').css('transform').split(' ')[5].slice(0, -1) / $(window).height() * 100
 
+
         if (id.currentTarget['id'] == 'up' && translateValue <= -60) {
-        translateValue += 60
-            $('hr#progress').width($('hr#progress').width() - hrWidth)
-            $('span#currentNumber').text(parseInt($('span#currentNumber').text()) - 1)
+            translateValue += 60
+            currentPage --
         }
+
 
         if (id.currentTarget['id'] == 'down' && translateValue > -60 * 18) {
             translateValue -= 60
-            $('hr#progress').width($('hr#progress').width() + hrWidth)
-            $('span#currentNumber').text(parseInt($('span#currentNumber').text()) + 1)
+            currentPage ++
         }
 
         $('.allPage').css('transform', 'translateY('+ translateValue +'vh)')
-
+        
     }
+    $('hr#progress').width(currentPage * hrWidth)
+    $('span#currentNumber').text(currentPage)
 })
